6e631f95e26486e18e69e90844ee7085e7653e44
Filipe Ilunga <filipeilunga@gmail.com>
2021-02-27 20:01:52 -0300

    Package DevicePreview excluido

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:agenda_de_contatos/View/homePage.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'View/contactPage.dart';
 
 void main() => runApp(
-      //    DevicePreview(
-      //     builder: (context) =>
       MyApp(),
-      //   enabled: !kReleaseMode,
-      //  ),
     );
 
 class MyApp extends StatelessWidget {
